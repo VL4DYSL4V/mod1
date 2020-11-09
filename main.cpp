3bb02d1efc86c85d4948a2a8a0af25327605f749
Vladislav <arzik66677@gmail.com>
2020-11-09 13:38:52 +0200

    4th task is fixed, fixed previous commit

--- main.cpp
+++ main.cpp
@@ -2,17 +2,38 @@
 #include <vector>
 #include "Nightingale.h"
 #include "Sparrow.h"
-
 //Ticket5;
 
 
+std::vector<int> getPair(){
+    std::vector<int> out;
+    int min1 = INT32_MAX;
+    int min2 = INT32_MAX;
+    int generalSum = 0;
+    while(generalSum <= 50){
+        std::cout<<"Input int number"<<std::endl;
+        int x;
+        std::cin>>x;
+        generalSum += x;
+        if(x < min1){
+            min2 = min1;
+            min1 = x;
+        }
+    }
+    out.push_back(min1);
+    out.push_back(min2);
+    return out;
+}
+
+void testPair(std::vector<int> result){
+    for(int i = 0; i<result.size(); i++){
+        std::cout<<result[i]<<std::endl;
+    }
+}
+
 void testBirds(){
     Nightingale nightingale("soooooong");
-//    nightingale.sing();
     Sparrow sparrow("soooooong");
-//    sparrow.sing();
-//
-//    std::cout<<"polimo"<<std::endl;
     std::vector<Bird*> birds;
     birds.push_back(&sparrow);
     birds.push_back(&nightingale);
