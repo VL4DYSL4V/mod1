800ab735395425169503c3e429e00795f8e7f1d4
Vladislav <arzik66677@gmail.com>
2020-11-09 14:20:41 +0200

    bugs in min func fixed

--- main.cpp
+++ main.cpp
@@ -4,46 +4,54 @@
 #include "Sparrow.h"
 //Ticket5;
 
-
-std::vector<int> getPair(){
+std::vector<int> getPair() {
     std::vector<int> out;
     int min1 = INT32_MAX;
     int min2 = INT32_MAX;
-    int generalSum = 0;
-    while(generalSum <= 50){
-        std::cout<<"Input int number"<<std::endl;
-        int x;
-        std::cin>>x;
-        generalSum += x;
-        if(x < min1){
-            min2 = min1;
-            min1 = x;
-        }
-    }
     out.push_back(min1);
     out.push_back(min2);
+    int generalSum = 0;
+    while (generalSum <= 50) {
+        std::cout << "Input int number" << std::endl;
+        int x;
+        std::cin >> x;
+        generalSum += x;
+        if (x < out[0] || x < out[1]) {
+            if(x<out[0]){
+                if(out[0] < out[1]){
+                    out[1] = out[0];
+                }
+                out[0] = x;
+            }else{
+                if(out[1] < out[0]){
+                    out[0] = out[1];
+                }
+                out[1] = x;
+            }
+        }
+    }
     return out;
 }
 
-void testPair(std::vector<int> result){
-    for(int i = 0; i<result.size(); i++){
-        std::cout<<result[i]<<std::endl;
+void testPair(std::vector<int> result) {
+    for (int i = 0; i < result.size(); i++) {
+        std::cout << result[i] << std::endl;
     }
 }
 
-void testBirds(){
+void testBirds() {
     Nightingale nightingale("soooooong");
     Sparrow sparrow("soooooong");
-    std::vector<Bird*> birds;
+    std::vector<Bird *> birds;
     birds.push_back(&sparrow);
     birds.push_back(&nightingale);
-    for(int i = 0; i < birds.size(); i++){
+    for (int i = 0; i < birds.size(); i++) {
         (*birds[i]).sing();
     }
 }
 
 int main() {
-//    testPair(getPair());
+    testPair(getPair());
     testBirds();
     return 0;
 }
